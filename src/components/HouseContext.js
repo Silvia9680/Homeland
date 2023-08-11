e4372b89c3fe29422c18c8315f099c46499b53f5
Silvia9680 <silviacarolina9680@gmail.com>
2023-08-11 14:19:45 -0300

    Add CountryDropdown

--- src/components/HouseContext.js
+++ src/components/HouseContext.js
@@ -13,6 +13,17 @@ const HouseContextProvider = ({children}) => {
   const [ price, setPrice ] = useState('Price range (any)');
   const [ loading, setLoading ] = useState(false);
 
+  useEffect(() => {
+    const allCountries = houses.map(( house ) => {
+      return house.country;
+    })
+
+    const uniqueCountries = ['Location (any)', ... 
+    new Set(allCountries)];
+
+    setCountries(uniqueCountries);
+  }, [])
+
   return(
   <HouseContext.Provider value={{
     country,
